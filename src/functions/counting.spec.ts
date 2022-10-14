07dde8170aa6930ba94907b0605995624ecb740c
halim-boussada <halimboussada10@gmail.com>
2022-10-14 10:57:05 +0100

    refine

--- src/functions/counting.spec.ts
+++ src/functions/counting.spec.ts
@@ -2,7 +2,6 @@ import counting from "./counting";
 
 describe("testing counting function", function () {
   let countries1 = require("../../data");
-
   it("counting with should output same result", function () {
     expect(counting([...countries1])[0].name).toEqual("Uzuzozne [1]");
   });
--- src/functions/filtering.spec.ts
+++ src/functions/filtering.spec.ts
@@ -3,13 +3,9 @@ import filterByString from "./filtering";
 describe("testing filter function", function () {
   let countries1 = require("../../data");
   let countries2 = require("../../data");
-
   it("filterByString with 'ry' query should output same result length", function () {
-    console.log(countries1);
-
     const output = filterByString([...countries1], "ry");
     expect(output.length).toEqual(2);
-    console.log(output.length);
   });
 
   it("filterByString with 'xyxx' query should output same result length", function () {
